2db7d2d96c5b9c54e5583051edb7310e66c8ede7
Noah Alderton <nalderto@purdue.edu>
2018-10-20 02:52:34 -0400

    Slight change to News API attribution link

--- stock-wise/src/components/Footer/Footer.js
+++ stock-wise/src/components/Footer/Footer.js
@@ -6,7 +6,12 @@ class Footer extends Component {
     render() {
         return(
             <div className = "footerDiv">
-            <p className="footerText">Made by Noah Alderton, Zachory Birenbaum, Vidhur Kumar, and JT Singhal for Boilermake 2018.<br></br><FontAwesomeIcon icon={['fab', 'github']} /><a href="https://github.com/JTSinghal/StockWise"> Github</a></p>
+            <p className="footerText">Made by Noah Alderton, Zachory Birenbaum, Vidhur Kumar, and JT Singhal for Boilermake 2018.
+                <br></br>
+                Powered by <a href="https://newsapi.org/">News API</a>
+                <br></br>
+                <FontAwesomeIcon icon={['fab', 'github']} /><a href="https://github.com/JTSinghal/StockWise"> Github</a>
+            </p>
             </div>
         )
     }
