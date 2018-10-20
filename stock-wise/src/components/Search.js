14af55647aec51f38aa2b615afb0ffff2221a2cf
Noah Alderton <nalderto@purdue.edu>
2018-10-19 23:25:11 -0400

    Fix search box

--- stock-wise/src/components/Search.js
+++ stock-wise/src/components/Search.js
@@ -4,13 +4,16 @@ import './Search.css';
 class Table extends Component {
     render() {
         return (
+            <div class="center">
                     <input
                         placeholder="Search for..."
                         ref={input => this.search = input}
                         onChange={this.handleInputChange}
                         type='search'
                     />
+                    </div>
         )
+        
     }
 }
 
