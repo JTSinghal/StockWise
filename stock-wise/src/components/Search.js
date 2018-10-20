a2c469a7cdec69f1471e0c0c709f267a01d309c1
Vidhur Kumar <vidhur2k@pal-nat186-95-242.itap.purdue.edu>
2018-10-19 23:23:45 -0400

    Added bootstrap

--- stock-wise/src/components/Search.js
+++ stock-wise/src/components/Search.js
@@ -4,12 +4,12 @@ import './Search.css';
 class Table extends Component {
     render() {
         return (
-                    <input
-                        placeholder="Search for..."
-                        ref={input => this.search = input}
-                        onChange={this.handleInputChange}
-                        type='search'
-                    />
+            <input
+                placeholder="Search for..."
+                ref={input => this.search = input}
+                onChange={this.handleInputChange}
+                type='search'
+            />
         )
     }
 }
